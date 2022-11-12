1026c95ab308a65857a55392222e0afe5ff7e390
Jonathan do Ouro <j248364@dac.unicamp.br>
2022-11-12 17:59:06 -0300

    endpoints: recuperar a temperatura e precipitação de uma terra, encontrar a região de uma terra, listar todas terras

--- src/routes/lands.js
+++ src/routes/lands.js
@@ -3,6 +3,45 @@ const landsController = require("../controllers/lands");
 
 const router = express.Router();
 
+//cria uma nova terra no banco de dados
 router.post("/", landsController.createNewLand);
+
+//retorna todas terras vazias
+router.get("/empty", landsController.listEmptyLands);
+
+//retorna todas terras
+router.get("/", landsController.listLands);
+
+/*  Retorna a região de uma terra que tem o id passado na url
+
+    Exemplo de retorno:
+    {
+    "fkRegionLand": {
+        "id": 19,
+        "name": "Regiao 19",
+        "averageTemperature": 19.5,
+        "averagePrice": null,
+        "annualPrecipitation": 1275,
+        "populationSize": 1380,
+        "numberSchools": 33,
+        "createdAt": "2022-11-10T18:03:12.000Z",
+        "updatedAt": "2022-11-10T18:03:12.000Z"
+    }
+}
+*/
+router.get("/:id/region", landsController.regionLand);
+
+/*  Retorna a precipitação e a temṕeratura média de uma terra
+    que tem o id passado na url.
+
+    Exemplo de retorno:
+    {
+    "fkRegionLand": {
+        "averageTemperature": 19.5,
+        "annualPrecipitation": 1275
+    }
+}
+*/
+router.get("/:id/precipitationAndTemperature", landsController.precipitationAndTemperature);
 
 module.exports = router;
--- src/services/lands.js
+++ src/services/lands.js
@@ -1,4 +1,5 @@
-const { Land } = require("../models");
+const { Land, Region } = require("../models");
+const { Op } = require("sequelize");
 
 const createNewLand = (req_body) => Land.create({
     "state": req_body.state, 
@@ -9,6 +10,39 @@ const createNewLand = (req_body) => Land.create({
     }
 );
 
+const findAllLands = () =>
+	Land.findAll({
+		attributes: ["state", "city", "address", "hectare"],
+	}
+);
+
+const findAllAvalibleLands = () =>
+	Land.findAll({
+		attributes: ["state", "city", "address", "hectare"],
+		where: {
+            familyid: {
+                [Op.is]: null
+            }
+        }
+	}
+);
+
+const getRegionLandById = (id) =>
+	Land.findByPk(id, {
+		attributes: [],
+		include: [{ model: Region, as: "fkRegionLand" }],
+	});
+
+const getPrecipitationAndTemperature = (id) =>
+    Land.findByPk(id, {
+        attributes: [],
+        include: [{ model: Region, as: "fkRegionLand", attributes: ["averageTemperature", "annualPrecipitation"] }],
+    })
+
 module.exports = {
 	createNewLand,
+    findAllAvalibleLands,
+    getRegionLandById,
+    findAllLands,
+    getPrecipitationAndTemperature,
 };
