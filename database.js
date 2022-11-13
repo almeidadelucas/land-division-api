2320cfaccbe86579118f7d18edec738ea35f0696
pvskp <pvincius14@gmail.com>
2022-11-13 15:14:52 -0300

    Implements env vars to use Docker compose

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const env = process.env.NODE_ENV || "development";
-const config = require("./config/database.json")[env];
+const config = require("./config/database.js")[env];
 
 const sequelize = new Sequelize(config);
 
