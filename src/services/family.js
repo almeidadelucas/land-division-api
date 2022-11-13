14d92ee68c142c3882e3cd90763252f1d25f25f3
pvskp <pvincius14@gmail.com>
2022-11-13 17:06:50 -0300

    Fix second merge conflict

--- src/services/family.js
+++ src/services/family.js
@@ -1,10 +1,28 @@
+const { client } = require("../../redis-server");
 const { Family, Member } = require("../models");
 
-const findAllFamilies = () =>
-	Family.findAll({
+const findAllFamilies = async () => {
+    const result = await client.get("families");
+    console.log(result);
+    if (result) {
+        console.log("Result returned by redis")
+        return JSON.parse(result)
+    }
+
+	const data = await Family.findAll({
+		attributes: ["id", "name"],
 		include: [{ model: Member, as: "fkMemberFamily" }],
 	});
+    
+    console.log("data:", data)
+    if (data) {
+        await client.set("families", JSON.stringify(data));
+        console.log("Content cached")
+    }
+    return data;
+};
+
 
 module.exports = {
 	findAllFamilies,
-};
+}
