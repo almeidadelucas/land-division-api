b413ff8cfadec5ec9062b306d6f90c439787c0a6
pvskp <pvincius14@gmail.com>
2022-11-13 17:20:44 -0300

    Fix third merge conflict

--- src/services/lands.js
+++ src/services/lands.js
@@ -1,18 +1,40 @@
+const { client } = require("../../redis-server");
 const { Land, Region } = require("../models");
 const { Op } = require("sequelize");
 
-const createNewLand = (req_body) =>
-	Land.create({
-		state: req_body.state,
-		regionId: req_body.regionId,
-		city: req_body.city,
-		address: req_body.address,
-		hectare: req_body.hectare,
-	});
-
-const findAllLands = () => Land.findAll();
-
 const findById = (id) => Land.findByPk(id);
+
+const createNewLand = async (req_body) => {
+    const data = await Land.create({
+        state: req_body.state, 
+        regionId: req_body.regionId,
+        city: req_body.city, 
+        address: req_body.address,
+        hectare: req_body.hectare
+        }
+    );
+
+    await client.del("lands")
+    console.log("Cache cleared")
+    return data
+}
+
+const findAllLands = async () => {
+    const result = await client.get("lands");
+    if (result) {
+        console.log("Result returned by redis")
+        return JSON.parse(result)
+    }
+
+    const data = await Land.findAll();
+    
+    if (data) {
+        await client.set("lands", JSON.stringify(data));
+        console.log("Content cached")
+    }
+    return data;
+
+}
 
 const findAllAvalibleLands = () =>
 	Land.findAll({
