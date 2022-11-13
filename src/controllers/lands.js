14ec84fba0a84ed782210b6188fc2bf868d6c531
Lucas de Almeida <almeidadelucas@gmail.com>
2022-11-12 23:44:15 -0300

    new routes and refactoing

--- src/controllers/lands.js
+++ src/controllers/lands.js
@@ -2,9 +2,19 @@ const landsService = require("../services/lands");
 
 const createNewLand = async (req, res) => {
 	try {
-        console.log(req.body)
+		console.log(req.body);
 		await landsService.createNewLand(req.body);
-		res.status(200).json({"status":"Ok"});
+		res.status(200).json({ status: "Ok" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+};
+
+const deleteLand = async (req, res) => {
+	try {
+		const id = req.params.id;
+		await landsService.deleteLand(id);
+		res.status(200).json({ status: "Ok" });
 	} catch (err) {
 		res.status(500).json({ message: err.message });
 	}
@@ -28,9 +38,19 @@ const listLands = async (req, res) => {
 	}
 };
 
+const getLandById = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const land = await landsService.findById(id);
+		res.status(200).json(land);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+};
+
 const regionLand = async (req, res) => {
 	try {
-		const id = req.params.id
+		const id = req.params.id;
 		const region = await landsService.getRegionLandById(id);
 		res.status(200).json(region);
 	} catch (err) {
@@ -40,7 +60,7 @@ const regionLand = async (req, res) => {
 
 const precipitationAndTemperature = async (req, res) => {
 	try {
-		const id = req.params.id
+		const id = req.params.id;
 		const region = await landsService.getPrecipitationAndTemperature(id);
 		res.status(200).json(region);
 	} catch (err) {
@@ -48,12 +68,36 @@ const precipitationAndTemperature = async (req, res) => {
 	}
 };
 
+const updateLand = async (req, res) => {
+	try {
+		const id = req.params.id;
+		const { state, city, address, hectare, agentId, familyId, regionId } =
+			req.body;
 
+		await landsService.updatedLand(id, {
+			state,
+			city,
+			address,
+			hectare,
+			agentId,
+			familyId,
+			regionId,
+		});
 
-module.exports = { 
-	createNewLand, 
-	listEmptyLands, 
-	regionLand, 
+		const updatedLand = await landsService.findById(id);
+		res.status(200).json(updatedLand);
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+};
+
+module.exports = {
+	createNewLand,
+	updateLand,
+	listEmptyLands,
+	getLandById,
+	regionLand,
 	listLands,
-	precipitationAndTemperature
+	precipitationAndTemperature,
+	deleteLand,
 };
--- src/services/lands.js
+++ src/services/lands.js
@@ -1,31 +1,28 @@
 const { Land, Region } = require("../models");
 const { Op } = require("sequelize");
 
-const createNewLand = (req_body) => Land.create({
-    "state": req_body.state, 
-    "regionId": req_body.regionId,
-    "city": req_body.city, 
-    "address": req_body.address,
-    "hectare": req_body.hectare
-    }
-);
+const createNewLand = (req_body) =>
+	Land.create({
+		state: req_body.state,
+		regionId: req_body.regionId,
+		city: req_body.city,
+		address: req_body.address,
+		hectare: req_body.hectare,
+	});
 
-const findAllLands = () =>
-	Land.findAll({
-		attributes: ["state", "city", "address", "hectare"],
-	}
-);
+const findAllLands = () => Land.findAll();
+
+const findById = (id) => Land.findByPk(id);
 
 const findAllAvalibleLands = () =>
 	Land.findAll({
 		attributes: ["state", "city", "address", "hectare"],
 		where: {
-            familyid: {
-                [Op.is]: null
-            }
-        }
-	}
-);
+			familyid: {
+				[Op.is]: null,
+			},
+		},
+	});
 
 const getRegionLandById = (id) =>
 	Land.findByPk(id, {
@@ -34,15 +31,28 @@ const getRegionLandById = (id) =>
 	});
 
 const getPrecipitationAndTemperature = (id) =>
-    Land.findByPk(id, {
-        attributes: [],
-        include: [{ model: Region, as: "fkRegionLand", attributes: ["averageTemperature", "annualPrecipitation"] }],
-    })
+	Land.findByPk(id, {
+		attributes: [],
+		include: [
+			{
+				model: Region,
+				as: "fkRegionLand",
+				attributes: ["averageTemperature", "annualPrecipitation"],
+			},
+		],
+	});
+
+const updatedLand = (id, body) => Land.update(body, { where: { id } });
+
+const deleteLand = (id) => Land.destroy({ where: { id } });
 
 module.exports = {
 	createNewLand,
-    findAllAvalibleLands,
-    getRegionLandById,
-    findAllLands,
-    getPrecipitationAndTemperature,
+	updatedLand,
+	deleteLand,
+	findAllAvalibleLands,
+	getRegionLandById,
+	findAllLands,
+	findById,
+	getPrecipitationAndTemperature,
 };
